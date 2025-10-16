1dee49e7481a108708c872684a651de22a657bf0
drcreatives <danielerheriada@gmail.com>
2025-10-16 21:10:04 +0100

    refactor: update Index component layout and add background image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,24 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ImageSourcePropType, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg as ImageSourcePropType}
+        className="absolute z-0 inset-0 w-full h-full"
+      />
+      <ScrollView
+        className="flex-1 px-5"
+        contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}
+        showsVerticalScrollIndicator={false}
+      >
+        <Image
+          source={icons.logo as ImageSourcePropType}
+          className="w-12 h-10 mx-auto mt-20 mb-5"
+        />
+      </ScrollView>
     </View>
   );
 }
