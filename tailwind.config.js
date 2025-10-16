51d9421a8deff0a597eab8c7531e3d5c18577777
drcreatives <danielerheriada@gmail.com>
2025-10-16 03:45:20 +0100

    Update tailwind.config.js to extend theme colors and include paths for Nativewind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,29 @@
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: [
-    "./App.tsx", 
+    "./App.tsx",
     "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      // Custom theme configurations go here
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#D6C6FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
+        },
+        dark: {
+          100: "#221F3D",
+          200: "#0F0D23",
+        },
+        accent: "#AB8BFF",
+      },
+    },
   },
   plugins: [],
-}
+};
